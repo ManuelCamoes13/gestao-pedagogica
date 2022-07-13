b355e905e8dc370a5992d67d7ed90208de86a973
Manuel Camões <manuelcamoes13@gmail.com>
2022-07-13 10:43:41 +0200

    ok

--- http-common.js
+++ http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL:'http://localhost:3333/api',
+    baseURL:'https://gestaoapi.herokuapp.com',
     // baseURL:'https://gestaopedagogica.herokuapp.com/api'
 
 });
